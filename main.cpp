a9482c1e4f84979cc6a25d1180d970c9182a4890
Matej Genci <matej.genci@gmail.com>
2025-10-03 07:54:17 +0100

    Use a buffer to draw the triangle

--- main.cpp
+++ main.cpp
@@ -26,6 +26,18 @@ int main() {
         return -1;
     }
 
+    float triangle_positions[3][2] = {
+        {-0.5f, -0.5f}, {0.5f, -0.5f}, {0, 0.5f}
+    };
+
+    GLuint buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_positions), triangle_positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
     std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
 
     /* Loop until the user closes the window */
@@ -34,18 +46,7 @@ int main() {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-
-        glColor3f(1.0f, 0.0f, 0.0f);
-        glVertex2f(-0.5f, -0.5f);
-
-        glColor3f(0.0f, 1.0f, 0.0f);
-        glVertex2f(0.5f, -0.5f);
-
-        glColor3f(0.0f, 0.0f, 1.0f);
-        glVertex2f(0, 0.5f);
-
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
